adeac41ab9b3cbfe4a9dd202f1c3e7c70822e6bf
giedrius <giedriustum@gmail.com>
2020-06-24 11:38:14 +0300

    catalog bundle with the first entity

---
+++ gtsrc/Catalog/CatalogBundle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: giedrius
+ * Date: 20.6.24
+ * Time: 11.28
+ */
+
+namespace Gt\Catalog;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CatalogBundle extends Bundle
+{
+
+}
---
+++ gtsrc/Catalog/Entity/Product.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: giedrius
+ * Date: 20.6.24
+ * Time: 11.29
+ */
+
+namespace Gt\Catalog\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="products")
+ */
+
+class Product
+{
+    /**
+     * @var string
+     * @ORM\Id
+     * @ORM\Column(type="string", length=32)
+     */
+    private $sku;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(type="datetime")
+     *
+     */
+    private $lastUpdate;
+
+    /**
+     * @return string
+     */
+    public function getSku(): string
+    {
+        return $this->sku;
+    }
+
+    /**
+     * @param string $sku
+     */
+    public function setSku(string $sku): void
+    {
+        $this->sku = $sku;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getLastUpdate(): \DateTime
+    {
+        return $this->lastUpdate;
+    }
+
+    /**
+     * @param \DateTime $lastUpdate
+     */
+    public function setLastUpdate(\DateTime $lastUpdate): void
+    {
+        $this->lastUpdate = $lastUpdate;
+    }
+
+}
