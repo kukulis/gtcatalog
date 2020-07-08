9bfa5cd6c7f58480e31ba8a862b7e115e38568b3
Tadas <tdavidsonas88@gmail.com>
2020-07-08 13:12:55 +0300

    LanguageDao.php (tokiu pat stiliumi kaip ir CatalogDao) - atskiras failas del Single Responsibility Principle (SOLID principai) ir LanguagesController

---
+++ gtsrc/Catalog/Controller/LanguagesController.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Gt\Catalog\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LanguagesController extends AbstractController
+{
+
+}
---
+++ gtsrc/Catalog/Dao/LanguageDao.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace Gt\Catalog\Dao;
+
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\ORM\EntityManager;
+use Gt\Catalog\Entity\Language;
+use Psr\Log\LoggerInterface;
+
+class LanguageDao
+{
+
+    /** @var LoggerInterface */
+    private $logger;
+
+    /** @var Registry */
+    private $doctrine;
+
+    /**
+     * LanguageDao constructor.
+     * @param LoggerInterface $logger
+     * @param Registry $doctrine
+     */
+    public function __construct(LoggerInterface $logger, Registry $doctrine)
+    {
+        $this->logger = $logger;
+        $this->doctrine = $doctrine;
+    }
+
+    public function getLanguagesList()
+    {
+        $languageClass = Language::class;
+        $dql = /** @lang DQL */ "SELECT l from $languageClass l";
+
+        /** @var EntityManager $em */
+        $em = $this->doctrine->getManager();
+
+        $languages = $em->createQuery($dql)->execute();
+        return $languages;
+    }
+}
