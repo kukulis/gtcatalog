11898f8d96ea5468ed1a2b72d6b41d24ea9864d7
Tadas <tdavidsonas88@gmail.com>
2020-07-08 17:10:15 +0300

    getting excited with Symfony forms ! Yaaayyyy baby :)

--- gtsrc/Catalog/Controller/LanguagesController.php
+++ gtsrc/Catalog/Controller/LanguagesController.php
@@ -12,6 +12,7 @@ class LanguagesController extends AbstractController
 
     public function newAction()
     {
-        return new Response('todo: finish to code add new language action');
+        return new Response('todo: finish to code add new language action with Symfony forms !
+         Getting excited =) ');
     }
 }
