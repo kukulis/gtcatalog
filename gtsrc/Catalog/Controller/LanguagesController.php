37cd1e9c056fc0dde9b61e5dd7d101d1ef7eff6d
Tadas <tdavidsonas88@gmail.com>
2020-07-10 10:15:42 +0300

    fixex for block body to display the form

--- gtsrc/Catalog/Controller/LanguagesController.php
+++ gtsrc/Catalog/Controller/LanguagesController.php
@@ -11,11 +11,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class LanguagesController extends AbstractController
 {
 
+    /**
+     * @param EntityManagerInterface $em
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function newAction(EntityManagerInterface $em)
     {
         $form = $this->createForm(LanguageFormType::class);
 
-        return $this->render('@Catalog/languages/languages.html.twig', [
+        return $this->render('@Catalog/languages/new.html.twig', [
             'languageForm' => $form->createView(),
         ]);
     }
