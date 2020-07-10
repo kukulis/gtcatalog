baae2fef04503a6493f770388f191b16d39cf5f2
Tadas <tdavidsonas88@gmail.com>
2020-07-10 13:19:22 +0300

    Lietuviški vertimai

--- gtsrc/Catalog/Controller/LanguagesController.php
+++ gtsrc/Catalog/Controller/LanguagesController.php
@@ -7,17 +7,24 @@ namespace Gt\Catalog\Controller;
 use Doctrine\ORM\EntityManagerInterface;
 use Gt\Catalog\Form\LanguageFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class LanguagesController extends AbstractController
 {
 
     /**
      * @param EntityManagerInterface $em
+     * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function newAction(EntityManagerInterface $em)
+    public function newAction(EntityManagerInterface $em, Request $request)
     {
         $form = $this->createForm(LanguageFormType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            dd($form->getData());
+        }
 
         return $this->render('@Catalog/languages/new.html.twig', [
             'languageForm' => $form->createView(),
