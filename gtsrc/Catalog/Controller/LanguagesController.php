6f8451b4f0e22c288513eb247fdfec0ddd892e61
Tadas <tdavidsonas88@gmail.com>
2020-07-10 16:16:27 +0300

    code adding new language to the database

--- gtsrc/Catalog/Controller/LanguagesController.php
+++ gtsrc/Catalog/Controller/LanguagesController.php
@@ -5,6 +5,7 @@ namespace Gt\Catalog\Controller;
 
 
 use Doctrine\ORM\EntityManagerInterface;
+use Gt\Catalog\Entity\Language;
 use Gt\Catalog\Form\LanguageFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -27,11 +28,23 @@ class LanguagesController extends AbstractController
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            dd($form->getData());
+            $data = $form->getData();
+            $language = new Language();
+            $language->setCode($data['code']);
+            $language->setName($data['name']);
+            $language->setLocaleCode($data['localeCode']);
+
+            $em->persist($language);
+            $em->flush();
         }
 
         return $this->render('@Catalog/languages/new.html.twig', [
             'languageForm' => $form->createView(),
         ]);
+    }
+
+    public function languages()
+    {
+        
     }
 }
