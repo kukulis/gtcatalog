1a773ea44013cb27632edf727b7cd0aacd2a9c72
Giedrius Tumelis <giedrius.tumelis@unishop.lt>
2020-10-22 14:30:54 +0300

    Skeleton for importing pictures.

---
+++ gtsrc/Catalog/Command/ImportTmpDataFromRestCommand.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * ImportTmpDataFromRestCommand.php
+ * Created by Giedrius Tumelis.
+ * Date: 2020-10-21
+ * Time: 15:08
+ */
+
+namespace Gt\Catalog\Command;
+
+
+/**
+ * Class ImportTmpDataFromRestCommand
+ * @package Gt\Catalog\Command
+ *
+ * We assume that there is a table tmp_skus.
+ *
+ */
+class ImportTmpDataFromRestCommand
+{
+
+}
