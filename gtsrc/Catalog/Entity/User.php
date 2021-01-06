bf7837a9a90a6f8b6d5d0cc7a8716c8edc24ae47
Giedrius Tumelis <giedrius.tumelis@unishop.lt>
2021-01-06 08:38:58 +0200

    Hotfix user name may be null

--- gtsrc/Catalog/Entity/User.php
+++ gtsrc/Catalog/Entity/User.php
@@ -45,7 +45,7 @@ class User implements UserInterface
 
     /**
      * @var string The hashed password
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string", nullable=true)
      */
     private $name;
 
