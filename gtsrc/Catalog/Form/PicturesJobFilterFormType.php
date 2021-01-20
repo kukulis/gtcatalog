82b85119933ca164ec40b96276f508f73d8293f3
Giedrius Tumelis <giedrius.tumelis@unishop.lt>
2021-01-20 10:54:02 +0200

    Finished jobs filter form.

--- gtsrc/Catalog/Form/PicturesJobFilterFormType.php
+++ gtsrc/Catalog/Form/PicturesJobFilterFormType.php
@@ -11,11 +11,24 @@ namespace Gt\Catalog\Form;
 
 use Gt\Catalog\Data\IPicturesJobsFilter;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class PicturesJobFilterFormType extends AbstractType implements IPicturesJobsFilter
 {
     private $limit=20;
     private $status;
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('limit'    , TextType::class, ['required'=>false])
+            ->add('status' , TextType::class, ['required'=>false] )
+            ->add('show'         , SubmitType::class, ['label'=>'Rodyti'])
+        ;
+    }
+
 
     /**
      * @return mixed
