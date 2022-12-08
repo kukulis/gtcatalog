52fddd93cffe5db67e8111af804fa8c0c718479f
Giedrius Tumelis <giedrius@inkodus.lt>
2022-12-08 09:12:28 +0200

    info endpoint

--- gtsrc/Catalog/Controller/HomeController.php
+++ gtsrc/Catalog/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace Gt\Catalog\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -16,5 +17,15 @@ class HomeController extends AbstractController
     {
         return $this->render('@Catalog/home/home.html.twig',[
         ]);
+    }
+
+    /**
+     * @Route("/info", name="home")
+     */
+    public function infoAction()
+    {
+        phpinfo();
+
+        return new Response('');
     }
 }
