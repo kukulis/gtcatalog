216adfb46098be870bdf5887c8b52b33f94336a2
Giedrius Tumelis <giedrius@inkodus.lt>
2023-03-08 12:43:49 +0200

    Deploy skriptas su migracijos iškvietimu.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,10 +4,16 @@ set -e
 echo -e "\n#$(date +"%Y-%m-%d %H:%M:%S") Atnaujinamas katalogas \n"
 git pull
 
+
+echo -e "\n#$(date +"%Y-%m-%d %H:%M:%S") Leidžiam migracijas \n"
+php bin/console doctrine:migrations:migrate --no-interaction
+
+
 echo -e "\n#$(date +"%Y-%m-%d %H:%M:%S") Išvalomas podėlis \n"
 php bin/console cache:clear
 
 echo -e "\n# $(date +"%Y-%m-%d %H:%M:%S") Atnaujimas cron \n"
 crontab cron/prod.conf
+
 
 echo -e "\n#$(date +"%Y-%m-%d %H:%M:%S") Laiminga pabaiga \n"
