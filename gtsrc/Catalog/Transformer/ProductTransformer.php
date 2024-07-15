81d218e1315c6c7342a3d464209624955b824451
Giedrius Tumelis <giedrius@inkodus.lt>
2024-07-15 11:17:58 +0300

    #dev-427 : simple test case for transforming product special fields.

--- gtsrc/Catalog/Transformer/ProductTransformer.php
+++ gtsrc/Catalog/Transformer/ProductTransformer.php
@@ -3,13 +3,17 @@
 namespace Gt\Catalog\Transformer;
 
 use Catalog\B2b\Common\Data\Catalog\Product as CatalogProduct;
+use Gt\Catalog\Entity\PackageType;
 use Gt\Catalog\Entity\Product;
 use Gt\Catalog\Entity\ProductLanguage;
+use Gt\Catalog\Entity\ProductPackage;
+use Gt\Catalog\Utils\MapBuilder;
 use Gt\Catalog\Utils\ProductsHelper;
-use Psr\Log\LoggerInterface;
 
-class ProductTransformer {
-    public function transformToRestProduct(ProductLanguage $productLanguage): CatalogProduct {
+class ProductTransformer
+{
+    public function transformToRestProduct(ProductLanguage $productLanguage): CatalogProduct
+    {
         $restProduct = new CatalogProduct();
         $this->mapProductToRestProduct($productLanguage->getProduct(), $restProduct);
         $this->mapProductLanguageToRestProduct($productLanguage, $restProduct);
@@ -18,13 +22,36 @@ class ProductTransformer {
     }
 
     // TODO check if works without &
-    private function mapProductToRestProduct(Product $product, CatalogProduct $restProduct): void {
+    private function mapProductToRestProduct(Product $product, CatalogProduct $restProduct): void
+    {
         $directMappings = [
-            'sku', 'version', 'brand', 'line', 'parentSku', 'originCountryCode',
-            'vendor', 'manufacturer', 'color', 'forMale', 'forFemale', 'size',
-            'packSize', 'packAmount', 'weight', 'weightBruto', 'length', 'height', 'width',
-            'deliveryTime', 'depositCode', 'codeFromCustom', 'guaranty', 'codeFromSupplier',
-            'codeFromVendor', 'priority', 'googleProductCategoryId'
+            'sku',
+            'version',
+            'brand',
+            'line',
+            'parentSku',
+            'originCountryCode',
+            'vendor',
+            'manufacturer',
+            'color',
+            'forMale',
+            'forFemale',
+            'size',
+            'packSize',
+            'packAmount',
+            'weight',
+            'weightBruto',
+            'length',
+            'height',
+            'width',
+            'deliveryTime',
+            'depositCode',
+            'codeFromCustom',
+            'guaranty',
+            'codeFromSupplier',
+            'codeFromVendor',
+            'priority',
+            'googleProductCategoryId'
         ];
 
         foreach ($directMappings as $property) {
@@ -34,14 +61,20 @@ class ProductTransformer {
             }
         }
 
-        $restProduct->lastUpdate = ProductsHelper::getFormattedDate($product->getLastUpdate(), ProductsHelper::DATE_FORMAT);
+        $restProduct->lastUpdate = ProductsHelper::getFormattedDate(
+            $product->getLastUpdate(),
+            ProductsHelper::DATE_FORMAT
+        );
         $restProduct->type = ProductsHelper::getClassificatorCode($product->getType());
         $restProduct->purpose = ProductsHelper::getClassificatorCode($product->getPurpose());
         $restProduct->measure = ProductsHelper::getClassificatorCode($product->getMeasure());
         $restProduct->productgroup = ProductsHelper::getClassificatorCode($product->getProductgroup());
     }
 
-    private function mapProductLanguageToRestProduct(ProductLanguage $productLanguage, CatalogProduct $restProduct): void {
+    private function mapProductLanguageToRestProduct(
+        ProductLanguage $productLanguage,
+        CatalogProduct $restProduct
+    ): void {
         $restProduct->language = $productLanguage->getLanguage()->getCode();
         $restProduct->name = $productLanguage->getName();
         $restProduct->description = $productLanguage->getDescription();
@@ -56,10 +89,68 @@ class ProductTransformer {
 
     /**
      * Updates db product from dto product. The updated fields written in the array and returned for logging purposes.
+     * Fields for updating:
+     * - weight
+     * - weight_bruto
+     * - code_from_custom
+     * - packages
+     *
+     * @param PackageType[] $packagesTypes
      *
      * @return string[] updated fields names
+     *
      */
-    public static function updateSpecialProduct( \Catalog\B2b\Common\Data\Catalog\Product $dtoProduct, Product $dbProduct) : array {
-        return [];
+    public static function updateSpecialProduct(
+        \Catalog\B2b\Common\Data\Catalog\Product $dtoProduct,
+        Product $dbProduct,
+        array $packagesTypes
+    ): array {
+        $updatedFields = [];
+
+        if ($dtoProduct->weight > 0 && $dbProduct->getWeight() == 0) {
+            $dbProduct->setWeight($dtoProduct->weight);
+
+            $updatedFields[] = 'weight';
+        }
+
+        if ($dtoProduct->weightBruto > 0 && $dbProduct->getWeightBruto() == 0) {
+            $dbProduct->setWeightBruto($dtoProduct->weightBruto);
+
+            $updatedFields[] = 'weight_bruto';
+        }
+
+        if ($dtoProduct->codeFromCustom && empty($dbProduct->getCodeFromCustom())) {
+            $dbProduct->setCodeFromCustom($dtoProduct->codeFromCustom);
+
+            $updatedFields[] = 'code_from_custom';
+        }
+
+        if ($dtoProduct->getPackages() && count($dbProduct->getPackages()) == 0) {
+            /** @var PackageType[] $packagesTypesByCode */
+            $packagesTypesByCode = MapBuilder::buildMap($packagesTypes, fn(PackageType $type) => $type->getCode());
+
+            $productsPackages = [];
+
+            // TODO use lambda later
+            foreach ($dtoProduct->getPackages() as $package) {
+                if (!array_key_exists($package->typeCode, $packagesTypesByCode)) {
+                    continue;
+                }
+                $productPackage = new ProductPackage();
+
+                $productPackage->setWeight($package->weight);
+                $productPackage->setPackageType($packagesTypesByCode[$package->typeCode]);
+
+                $productsPackages[] = $productPackage;
+            }
+
+            if (count($productsPackages) != 0) {
+                $dbProduct->setPackages($productsPackages);
+
+                $updatedFields[] = 'packages';
+            }
+        }
+
+        return $updatedFields;
     }
 }
--- tests/Gt/Catalog/UpdateSpecialProductsTest.php
+++ tests/Gt/Catalog/UpdateSpecialProductsTest.php
@@ -3,7 +3,10 @@
 
 namespace App\Tests\Gt\Catalog;
 
+use Catalog\B2b\Common\Data\Catalog\Package;
 use Catalog\B2b\Common\Data\Catalog\Product;
+use Gt\Catalog\Entity\PackageType;
+use Gt\Catalog\Entity\ProductPackage;
 use Gt\Catalog\Transformer\ProductTransformer;
 use PHPUnit\Framework\TestCase;
 
@@ -12,25 +15,67 @@ class UpdateSpecialProductsTest extends TestCase
     /**
      * @dataProvider provideDataForUpdate
      *
+     * @param PackageType[] $packagesTypes
+     *
      * @param string[] $expectedFields
      */
-    public function testUpdate(Product $dto, \Gt\Catalog\Entity\Product $product, \Gt\Catalog\Entity\Product $expectedProduct, array $expectedFields ) {
-        $updatedFields = ProductTransformer::updateSpecialProduct($dto, $product);
+    public function testUpdate(
+        Product $dto,
+        \Gt\Catalog\Entity\Product $product,
+        array $packagesTypes,
+        \Gt\Catalog\Entity\Product $expectedProduct,
+        array $expectedFields
+    ) {
+        $updatedFields = ProductTransformer::updateSpecialProduct($dto, $product, $packagesTypes);
 
         $this->assertEquals($expectedFields, $updatedFields);
         $this->assertEquals($expectedProduct, $product);
     }
 
-    public static function provideDataForUpdate() : array {
+    public static function provideDataForUpdate(): array
+    {
         return [
             'test empty' => [
                 'dto' => new Product(),
                 'product' => new \Gt\Catalog\Entity\Product(),
+                'packagesTypes' => [],
                 'expectedProduct' => new \Gt\Catalog\Entity\Product(),
                 'expectedFields' => []
             ],
-
-            // TODO real data
+            'test real' => [
+                'dto' =>
+                    (new Product())
+                        ->setSku('abc')
+                        ->setWeight(4.5)
+                        ->setWeightBruto(4.6)
+                        ->setCodeFromCustom('123456')
+                        ->setPackages(
+                            [
+                                (new Package())->setTypeCode('glass')->setWeight(0.1)
+                            ]
+                        )
+                ,
+                'product' => (new \Gt\Catalog\Entity\Product())
+                    ->setSku('abc'),
+                'packagesTypes' => [
+                    (new PackageType())->setCode('glass')->setDescription('Stiklas')
+                ],
+                'expectedProduct' => (new \Gt\Catalog\Entity\Product())
+                    ->setSku('abc')
+                    ->setWeight(4.5)
+                    ->setWeightBruto(4.6)
+                    ->setCodeFromCustom('123456')
+                    ->setPackages(
+                        [
+                            (new ProductPackage())->setPackageType(
+                                (new PackageType())->setCode('glass')->setDescription('Stiklas')
+                            )
+                                ->setWeight(0.1)
+                        ]
+                    )
+                ,
+                'expectedFields' => ['weight', 'weight_bruto', 'code_from_custom', 'packages']
+            ],
         ];
     }
 
